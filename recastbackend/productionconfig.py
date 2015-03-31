339f37171915389085d77259cbebad794d45b7ab
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-03-31 16:07:44 -0400

    starting work to move produciton to backend module

---
+++ recastbackend/productionconfig.py
@@ -0,0 +1,6 @@
+BROKER_URL = 'redis'
+CELERY_RESULT_BACKEND = "redis"
+CELERY_REDIS_HOST = "recast-demo"
+CELERY_REDIS_PORT = 6379
+CELERY_REDIS_DB = 0
+CELERY_IMPORTS = ('recastbackend.backendtasks',)
