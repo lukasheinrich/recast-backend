1f68515e2fb6efce0e1dc75cf9d42d046ace6ca3
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-03-31 15:56:03 -0400

    basic CLI logic in backend.. to be used by both production and dev

---
+++ recastbackend/submitter.py
@@ -0,0 +1,49 @@
+# a method that can be used to write CLI to submit celery workflows to an app
+
+import click
+import importlib
+import redis
+import celery
+import msgpack
+import time
+
+def get_parents(child):
+  x = child.parent
+  while x:
+    yield x
+    x = x.parent
+
+def pubsub_or_ready(result,pubsub):
+  while True:
+    message = pubsub.get_message()
+    # if our result is ready (failed or successful)
+    # or one of the parents failed we're done
+    if result.ready() or any(map(lambda x:x.failed(),get_parents(result))):
+      return
+    if message:
+      yield message      
+    time.sleep(0.001)  # be nice to the system :)    
+
+def agnostic_submit(uuid,parameter,wrapper_func,queue,modulename):
+  analysis_module = importlib.import_module(modulename)
+  analysis_chain  = analysis_module.get_chain(queue)
+  j,c = wrapper_func(uuid,parameter,analysis_chain,analysis_module.resultlist,queue)
+  result = c.apply_async()
+  click.secho('submitted chain for module {}'.format(modulename))
+  
+  
+  red = redis.StrictRedis(host = celery.current_app.conf['CELERY_REDIS_HOST'],
+                            db = celery.current_app.conf['CELERY_REDIS_DB'], 
+                          port = celery.current_app.conf['CELERY_REDIS_PORT'])
+  pubsub = red.pubsub()
+  pubsub.subscribe('socket.io#emitter')
+  for m in pubsub_or_ready(result,pubsub):
+    if m['type'] == 'message':
+      data =  msgpack.unpackb(m['data'])[0]
+      extras =  msgpack.unpackb(m['data'])[1]
+      if(data['nsp'] == '/monitor'):
+        click.secho('received message: {date} -- {msg}'.format(**(data['data'][1])),fg = 'blue')
+  if result.successful():
+    click.secho('chain suceeded',fg = 'green')
+  else:
+    click.secho('chain failed somewhere',fg = 'red')
--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
   version = '0.0.1',
   packages = find_packages(),
   install_requires = [
-    'Celery'
+    'Celery',
+    'Click',
+    'redis'
   ]
 )
