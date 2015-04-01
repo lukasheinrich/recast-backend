4a98b01856e249dc40cb01ba252e7e895f797716
Lukas Heinrich <lukas.heinrich@cern.ch>
2015-04-01 18:05:38 +0200

    production submission cli

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
   name = 'recast-backend',
   version = '0.0.1',
   packages = find_packages(),
+  entry_points={
+        'console_scripts': ['recast-prodsub = recastbackend.submitcli:submit']
+      },
   install_requires = [
     'Celery',
     'Click',
