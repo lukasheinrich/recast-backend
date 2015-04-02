1d95de5ca62179a4cd13b88cfc7b43dc4cf64cbb
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-04-02 14:26:48 -0400

    result access methods

---
+++ recastbackend/resultaccess.py
@@ -0,0 +1,6 @@
+import os
+
+def resultfilepath(requestId,parameter_pt,backend,path):
+  content_path = os.environ['RECAST_CONTENT_PATH_TEMPL'].format(requestId,parameter_pt)
+  content_path += '/'+backend
+  return '{}/{}'.format(content_path,path)
