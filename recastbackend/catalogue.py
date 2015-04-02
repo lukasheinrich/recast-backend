ec0c1c9da822cad452b3519ed3b2580386785254
Lukas Heinrich <lukas.heinrich@cern.ch>
2015-04-02 22:08:20 +0200

    reenable dmhiggs and hype and rivet

--- recastbackend/catalogue.py
+++ recastbackend/catalogue.py
@@ -1,26 +1,20 @@
-#import recastdmhiggs.dmhiggs_backendtasks as dmhiggs_backendtasks
-#import recastdmhiggs.dmhiggs_blueprint as dmhiggs_blueprint
-#import recasthype.hype_backendtasks as hype_backendtasks
-#import recasthype.hype_blueprint as hype_blueprint
-
 implemented_analyses = {
-  # dmhiggs_blueprint.RECAST_ANALYSIS_ID :
-  #     {
-  #      'workflow':dmhiggs_backendtasks,
-  #      'queue':'dmhiggs_queue',
-  #      'blueprint':dmhiggs_blueprint.blueprint
-  #      },
-  # hype_blueprint.RECAST_ANALYSIS_ID :
-  #     {
-  #      'workflow':hype_backendtasks,
-  #      'queue':'hype_queue',
-  #      'blueprint':hype_blueprint.blueprint
-  #     },
+  '858fb12c-b62f-9954-1997-a6ff8c27be0e' :
+       {
+        'workflow':'recastdmhiggs.backendtasks',
+        'queue':'dmhiggs_queue',
+        'blueprint':'recastdmhiggs.blueprint:blueprint'
+        },
+  '3ad4efdb-0170-fb94-75a5-8a1279386745' :
+       {
+        'workflow':'recasthype.backendtasks',
+        'queue':'hype_queue',
+        'blueprint':'recasthype.blueprint:blueprint'
+       },
   '19c471ff-2514-eb44-0d82-59563cc38dab' :
       {
        'workflow':'recastsusyhiggs.backendtasks',
        'queue':'susy_queue',
        'blueprint':'recastsusyhiggs.blueprint:blueprint'
       }  
-
 }
