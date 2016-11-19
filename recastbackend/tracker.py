fb33881f8d58f3b720edb53fa456fbf11b1440a7
lukasheinrich <lukas.heinrich@gmail.com>
2016-11-19 17:52:13 +0100

    track without blocking

--- recastbackend/tracker.py
+++ recastbackend/tracker.py
@@ -8,14 +8,15 @@ from recastbackend.messaging import get_stored_messages
 
 @click.command()
 @click.argument('jobguid')
+@click.option('-e','--exit')
 @click.option('-c','--celeryapp',default = 'recastbackend.fromenvapp:app')
-def track(celeryapp,jobguid):
+def track(celeryapp,jobguid,exit):
     try:
         module,attr = celeryapp.split(':')
         mod = importlib.import_module(module)
         app = getattr(mod,attr)
         app.set_current()
-        
+
         stored = get_stored_messages(jobguid)
         click.secho('=====================',fg = 'black')
         click.secho('What happened so far:',fg = 'black')
@@ -23,15 +24,18 @@ def track(celeryapp,jobguid):
         for m in stored:
             msg   = click.style('{date} -- {msg}'.format(**json.loads(m)),fg = 'black')
             click.secho(msg)
-          
+
+        if exit:
+            return
+
         click.secho('=====================',fg = 'green')
         click.secho('Tuning in live at {}: '.format(datetime.datetime.now().strftime('%Y-%m-%d %X')), fg = 'green')
         click.secho('=====================',fg = 'green')
-        
+
         result = get_result_obj(jobguid)
         for msgdata,_ in yield_from_celery(app,jobguid, lambda: result.ready()):
             click.secho('{date} :: {msg}'.format(**msgdata))
-    
+
     except KeyboardInterrupt:
         click.secho('bye bye.')
         return
