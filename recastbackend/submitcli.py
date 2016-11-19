df63c38a06d2c24ba65025ea44c0ad830da689f3
lukasheinrich <lukas.heinrich@gmail.com>
2016-11-19 17:36:25 +0100

    enable preset pars

--- recastbackend/submitcli.py
+++ recastbackend/submitcli.py
@@ -1,4 +1,6 @@
 import click
+import os
+import yaml
 from recastbackend.fromenvapp import app
 
 from recastbackend.submission import submit_celery
@@ -19,17 +21,27 @@ def submit():
 @click.argument('workflow')
 @click.argument('outputs')
 @click.argument('outputdir')
+@click.option('-p','--presetyml', default = '')
 @click.option('-t','--toplevel', default = 'from-github/pseudocap')
 @click.option('-q','--queue', default = 'recast_cap_queue')
 @click.option('--track/--no-track',default = False)
-def yadage(input_url,workflow,outputs,outputdir,track,queue,toplevel):
+def yadage(input_url,workflow,outputs,outputdir,track,queue,toplevel,presetyml):
     outputdir = outputdir
     ctx = backendcontexts.common_context(input_url,outputdir,'fromcli')
     explicit_results = backendcontexts.generic_yadage_outputs() + outputs.split(',')
+
+    if presetyml:
+        toload = open(presetyml) if os.path.exists(presetyml) else presetyml
+        presetpars = yaml.load(toload)
+        if not type(presetpars) == dict:
+            raise click.ClickException(click.style('Sorry, but your presets don\'t appear to be a dictionary',fg = 'red'))
+    else:
+        presetpars = {}
+
     ctx = backendcontexts.yadage_context(
         ctx,workflow,
         toplevel,
-        {},
+        presetpars,
         explicit_results = explicit_results
     )
     result = submit_celery(ctx,queue)
