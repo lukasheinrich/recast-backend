25df4dbf021ddcbf5627bcd63184d7bb19e8cda0
lukasheinrich <lukas.heinrich@gmail.com>
2017-02-01 23:16:40 +0100

    live config option

--- recastbackend/recastconfig.py
+++ recastbackend/recastconfig.py
@@ -1,12 +1,17 @@
 import pkg_resources
 import os
 import yaml
+import urllib2
+
 
 def backendconfig():
-    defaultconfigfile = pkg_resources.resource_filename('recastbackend','resources/backendconfig.yml')
-    configfile = os.environ.get('RECAST_BACKENDCONFIGFILE',defaultconfigfile)
-    configdata = yaml.load(open(configfile))
-    return configdata
+	url = os.environ.get('RECAST_BACKENDCONFIGURL',None)
+	if not url:
+		defaultconfigfile = pkg_resources.resource_filename('recastbackend','resources/backendconfig.yml')
+		configfile = os.environ.get('RECAST_BACKENDCONFIGFILE',defaultconfigfile)
+		url = 'file://'+os.path.realpath(configfile)
+	configdata = yaml.load(urllib2.urlopen(url))
+	return configdata
 
 def yadage_result_config():
     yadagepluginconf = backendconfig()['plugin_configs']['yadageworkflow']
