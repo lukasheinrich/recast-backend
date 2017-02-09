cf727f12b640fae98260dd5ff53ef1ac9be998cf
lukasheinrich <lukas.heinrich@gmail.com>
2017-02-09 01:40:53 +0100

    Rivet Catalogue

--- recastbackend/catalogue.py
+++ recastbackend/catalogue.py
@@ -1,4 +1,41 @@
 from recastconfig import backendconfig
+import json
+import pkg_resources
+import copy
+
+def rivet_catalogue():
+    ''''this will spit out combo configs for all rivet analyses'''
+    template = {
+        'analysisid': None,
+        'configname': 'rivetanalysis',
+        'required_interface': 'hepmcfiles',
+        'config': {
+            'toplevel': 'from-github/phenoana/generic_rivet',
+            'workflow': 'rivetanflow.yml',
+            'preset_pars':{
+                'analysis': None
+            }
+        }
+    }
+    in2recast = json.load(open(pkg_resources.resource_filename(
+                        'recastbackend',
+                        'resources/inspire_to_recast.json')
+                ))
+    in2rivet  = json.load(open(pkg_resources.resource_filename(
+                        'recastbackend','resources/inspire_to_rivet.json')
+                ))
+
+    rivet_downstreams = []
+    for inspire_id, recast_analysis_id in in2recast.iteritems():
+        rivet_analyses = in2rivet[inspire_id]
+        for analysis in rivet_analyses + ['MC_GENERIC']:
+
+            specific = copy.deepcopy(template)
+            specific['analysisid'] = recast_analysis_id
+            specific['configname'] = 'rivet_{}'.format(analysis)
+            specific['config']['preset_pars'] = 'rivetanalysis'
+            rivet_downstreams.append(specific)
+    return rivet_downstreams
 
 def recastcatalogue():
     # for now we'll just reload the file each time later we might reference a database or public repo
@@ -40,7 +77,9 @@ def recastcatalogue():
         'config': 'from-request'
     }
 
-    for downstream in comboflowcfg['downstream_configs']:
+    downstream_cfgs = comboflowcfg['downstream_configs'] + rivet_catalogue()
+
+    for downstream in downstream_cfgs:
         anid = downstream['analysisid']
 
         for possible_upstream in [fromrequest]+upstream_by_iface.get(downstream['required_interface'],[]):
