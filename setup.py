24ddc5f1ba5752cb819227760f13d6484c8f03f4
lukasheinrich <lukas.heinrich@gmail.com>
2017-08-12 19:11:51 +0200

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
   name = 'recast-backend',
-  version = '0.0.1',
+  version = '0.1.0',
   packages = find_packages(),
   entry_points= {
         'console_scripts': [
@@ -20,6 +20,6 @@ setup(
     'glob2'
   ],
   dependency_links = [
-    'https://github.com/recast-hep/recast-api/tarball/master#egg=recast-api-0.0.1',
+    'https://github.com/recast-hep/recast-api/tarball/master#egg=recast-api-0.1.0',
   ]
 )
